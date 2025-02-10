bf1d3b392dbb7de4081e0610fad5443f577424f3
soapy-ear <w1904477@my.westminster.ac.uk>
2025-02-11 06:14:33 +0900

    fix front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,18 @@
 import React, { Fragment, useEffect, useState } from "react";
 import "./App.css";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
-
-// Components
-import Navbar from "./components/Navbar"; // Import Navbar
-import Dashboard from "./components/Dashboard";
-import Login from "./components/Login";
-import Register from "./components/Register";
-import ModReg from "./components/ModReg";
-import Error from "./components/Error";
-import Timetable from "./components/Timetable";
-import ManageModules from "./components/ManageModules";
-import ListModules from "./components/ListModules";
-import MyProfile from "./components/MyProfile";
-import ModuleDetails from "./components/ModuleDetails";
 
 function App() {
-  //const [message, setMessage] = useState("");
+  const [message, setMessage] = useState("");
   useEffect(() => {
     fetch("https://acatempolive-backend.onrender.com")
-      .then((res) => res.json());
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message));
   }, []);
   return (
-    <Fragment>
+    <div className="App">
+      <h1>{message}</h1>
       <h1>Welcome to AcaTempo</h1>
-   
-    </Fragment>
+    </div>
   );
 }
 
