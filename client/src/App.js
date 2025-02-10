4fa438305431963efb060871c0781bb1ef9c038c
soapy-ear <w1904477@my.westminster.ac.uk>
2025-02-11 04:46:00 +0900

    Remove uneeded line

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React, { Fragment, useEffect, useState } from "react";
-import { responsivePropType } from "react-bootstrap/esm/createUtilityClasses";
 import "./App.css";
 
 function App() {
