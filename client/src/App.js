03754b31a75b90e7b5889c92c2bf54c9a629154c
soapy-ear <w1904477@my.westminster.ac.uk>
2025-02-11 04:39:00 +0900

    live backend url

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,9 @@ import "./App.css";
 function App() {
   const [message, setMessage] = useState("");
   useEffect(() => {
-    fetch("http://localhost:5001").then(res => res.json()).then(data => setMessage(data.message));
+    fetch("https://acatempolive-backend.onrender.com")
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message));
   }, []);
   return (
   <div className="App">
